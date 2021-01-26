a49ad9e630efaa47e94eafd34d377e5c8c2aa7a6
jennyzhang7 <zyjenny31@gmail.com>
2021-01-26 14:22:19 -0500

    added error handling

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   const [results, setResults] = useState([]);
   const [longitude, setLongitude] = useState("");
   const [latitude, setLatitude] = useState("");
+  const [error, setError] = useState("");
 
   const [isFahrenheit, setIsFahrenheit] = useState(true);
 
   useEffect(() => {
     getLocation();
-  }, );
+  }, []);
 
   var x = document.getElementById("demo");
   function getLocation() {
@@ -19,7 +20,6 @@ function App() {
       navigator.geolocation.getCurrentPosition(showPosition);
       console.log("Longitude" + longitude);
       console.log("Latitude" + latitude);
-
     } else {
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
@@ -60,41 +60,52 @@ function App() {
   };
 
   const fetchWeatherData = async () => {
-    console.log(results.length)
+    // console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`)
     if (latitude !== "" || longitude !== "") {
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_API_KEY}`
       );
 
-      const data = await response.json();
+      const data = await response.json()
+
       console.log(data);
-      const dataPoint = {
-        temp: data.main.temp,
-        city: data.name,
-        icon: data.weather[0].icon,
-      };
-      setResults(dataPoint);
+      if (data.name) {
+        const dataPoint = {
+          temp: data.main.temp,
+          city: data.name,
+          icon: data.weather[0].icon,
+        };
+        setResults(dataPoint);
+      } else {
+        setError("API error");
+      }
+      
       console.log(results);
     }
   };
   return (
     <div className="App">
       <div id="demo">
-      {(results.length === 0) && (
+        {error.length > 0 && (
+          <h2> API error occurred. </h2>
+        )}
+        {results.length === 0 && error.length === 0 && (
           <>
             <img src={image} alt="dance"></img>
-            <h2>
-              Click on button below to check out today's weather forecast
-            </h2>
+            <h2>Click on button below to check out today's weather forecast</h2>
           </>
         )}
-        {(results.length !== 0) && (
+        {results.length !== 0 && (
           <>
             <img
               src={`http://openweathermap.org/img/wn/${results.icon}@2x.png`}
               alt="Logo"
             />
-            <h1>{results.city}</h1>
+            <h1>
+              {results.city.length > 0
+                ? results.city
+                : "Cannot detect your city"}
+            </h1>
             <h2>
               {getTemperature(results.temp)} {getMeasurement()}
             </h2>
